d4223f62626c964f438568183f9c47f96982be65
Edward <crazybber@outlook.com>
2020-05-09 17:54:28 +0800

    add a routes declaration

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,11 +1,15 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { Routes, RouterModule } from '@angular/router';
 
+const routes: Routes = [
 
+];
 
 @NgModule({
   declarations: [],
   imports: [
+    RouterModule.forRoot(routes),
     CommonModule
   ]
 })
