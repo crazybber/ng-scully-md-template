c2cfc68dc96493fc6269059c34569b5d25fdcae6
Edward <crazybber@outlook.com>
2020-05-09 18:42:14 +0800

    Inject ScullyRoutesService

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { ScullyRoutesService } from '@scullyio/ng-lib';
+import { Observable } from 'rxjs';
 
 @Component({
   selector: 'app-home',
@@ -6,10 +8,14 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./home.component.css']
 })
 export class HomeComponent implements OnInit {
-
-  constructor() { }
+  links$: Observable<any> = this.scully.available$;
+  constructor(private scully: ScullyRoutesService) { }
 
   ngOnInit(): void {
+    // debug current pages
+    this.links$.subscribe(links => {
+      console.log(links);
+    });
   }
 
 }
