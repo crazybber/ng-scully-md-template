bfef8e2ffc85f4102530c6de86261ed27a798f6d
Edward <crazybber@outlook.com>
2020-05-09 18:35:13 +0800

    default home module as app root

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -2,9 +2,10 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { Routes, RouterModule } from '@angular/router';
 
-const routes: Routes = [{ path: 'blog', loadChildren: () => import('./blog/blog.module').then(m => m.BlogModule) },
-
-{ path: 'home', loadChildren: () => import('./home/home.module').then(m => m.HomeModule) }
+const routes: Routes = [
+  { path: '', redirectTo: '/home', pathMatch: 'full' },
+  { path: 'blog', loadChildren: () => import('./blog/blog.module').then(m => m.BlogModule) },
+  { path: 'home', loadChildren: () => import('./home/home.module').then(m => m.HomeModule) }
 
 ];
 
