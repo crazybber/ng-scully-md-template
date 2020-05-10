c53d69541f4edbb3dd1f9c832f0cd7cc07f026cb
Edward <crazybber@outlook.com>
2020-05-10 14:35:52 +0800

    import markdown and http related modules

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,20 +1,40 @@
+import { HttpClient, HttpClientModule } from '@angular/common/http';
+import { NgModule, SecurityContext } from '@angular/core';
+import { FormsModule } from '@angular/forms';
 import { BrowserModule } from '@angular/platform-browser';
-import { NgModule } from '@angular/core';
 
-import { AppComponent } from './app.component';
 import { AppRoutingModule } from './app-routing.module';
 import { ScullyLibModule } from '@scullyio/ng-lib';
 
+import { MarkdownModule, MarkedOptions, MarkedRenderer } from 'ngx-markdown';
+
+import { AppComponent } from './app.component';
+
+export function markedOptions(): MarkedOptions {
+  const renderer = new MarkedRenderer();
+
+  renderer.blockquote = (text: string) => {
+    return '<blockquote class="blockquote"><p>' + text + '</p></blockquote>';
+  };
+
+  return { renderer };
+}
+
 @NgModule({
-  declarations: [
-    AppComponent
-  ],
+  declarations: [AppComponent],
   imports: [
     BrowserModule,
+    FormsModule,
     AppRoutingModule,
-    ScullyLibModule
+    ScullyLibModule,
+    HttpClientModule,
+    MarkdownModule.forRoot({
+      loader: HttpClient,
+      markedOptions: { provide: MarkedOptions, useFactory: markedOptions },
+      sanitize: SecurityContext.HTML, // default value
+    }),
   ],
   providers: [],
-  bootstrap: [AppComponent]
+  bootstrap: [AppComponent],
 })
-export class AppModule { }
+export class AppModule {}
