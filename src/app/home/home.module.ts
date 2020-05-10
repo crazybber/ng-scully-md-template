fab3e0940e6c7ddaf56b1ecacb319f77c954d69d
Edward <crazybber@outlook.com>
2020-05-10 17:32:06 +0800

    import  MarkdownModule to home module

--- src/app/home/home.module.ts
+++ src/app/home/home.module.ts
@@ -4,12 +4,10 @@ import { CommonModule } from '@angular/common';
 import { HomeRoutingModule } from './home-routing.module';
 import { HomeComponent } from './home.component';
 
+import { MarkdownModule } from 'ngx-markdown';
 
 @NgModule({
   declarations: [HomeComponent],
-  imports: [
-    CommonModule,
-    HomeRoutingModule
-  ]
+  imports: [CommonModule, HomeRoutingModule, MarkdownModule],
 })
-export class HomeModule { }
+export class HomeModule {}
